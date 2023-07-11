9f5ab660496ae19d6d450ad1f6370d5fc7fac086
Vishesh Shrivastava <vishesh24cs138@satiengg.in>
2023-07-11 07:27:47 +0530

    Some Updates

--- array/LongestConsecutiveSeq(Better&Optimal).cpp
+++ array/LongestConsecutiveSeq(Better&Optimal).cpp
@@ -17,28 +17,48 @@ public:
     //    currmax = max(currmax,cnt);
     //    return currmax;  
     
-    //Optimized
+    //Optimized Mine
         //using set O(n) 
+        // unordered_set s(nums.begin(),nums.end());
+        // if(s.size() == 0 || s.size() == 1)return s.size();
+        // int next = 0, prev = 0,ans = INT_MIN;
+        // int x;
+        // for(int i:nums){
+        //     x = i-1;
+        //     while(s.find(x) != s.end()){
+        //         prev++;
+        //         s.erase(x);
+        //         x--;
+        //     }
+        //     x = i+1;
+        //     while(s.find(x) != s.end()){
+        //         next++;
+        //         s.erase(x);
+        //         x++;
+        //     }
+        //     ans = max(ans,prev+next+1);
+        //     prev = 0,next = 0;
+        // } 
+        // return ans;
+
+        //Optimized O(2n)
         unordered_set s(nums.begin(),nums.end());
         if(s.size() == 0 || s.size() == 1)return s.size();
-        int next = 0, prev = 0,ans = INT_MIN;
+        int cnt = 0,ans = INT_MIN;
         int x;
-        for(int i:nums){
-            x = i-1;
-            while(s.find(x) != s.end()){
-                prev++;
-                s.erase(x);
-                x--;
+        for(auto i : s){
+            //if no prev exist
+            if(s.find(i-1) == s.end()){
+                cnt++;
+                x = i+1;
+                while(s.find(x)!=s.end()){
+                    cnt++;
+                    x++;
+                }
+                ans = max(cnt,ans);
             }
-            x = i+1;
-            while(s.find(x) != s.end()){
-                next++;
-                s.erase(x);
-                x++;
-            }
-            ans = max(ans,prev+next+1);
-            prev = 0,next = 0;
-        } 
+            cnt = 0;
+        }
         return ans;
     }
 };
