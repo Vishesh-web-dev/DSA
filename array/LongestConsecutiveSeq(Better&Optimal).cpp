cf8b5ba4559faa34fd7e2185125fadc3aba9b45a
Vishesh Shrivastava <vishesh24cs138@satiengg.in>
2023-07-11 07:05:21 +0530

    some update

---
+++ array/LongestConsecutiveSeq(Better&Optimal).cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+    //    //B.F. Sort and Check O(nlogn)
+    //    sort(nums.begin(),nums.end());
+    //    int cnt = 1;
+    //    int currmax = 1;
+    //    for(int i = 0 ; i < nums.size() - 1 ;i++){
+    //        if(nums[i] == nums[i+1])continue;
+    //        if(nums[i]+1 == nums[i+1]){
+    //            cnt++;
+    //        }else{
+    //            currmax = max(currmax,cnt);
+    //            cnt = 1;
+    //        }
+    //    }
+    //    currmax = max(currmax,cnt);
+    //    return currmax;  
+    
+    //Optimized
+        //using set O(n) 
+        unordered_set s(nums.begin(),nums.end());
+        if(s.size() == 0 || s.size() == 1)return s.size();
+        int next = 0, prev = 0,ans = INT_MIN;
+        int x;
+        for(int i:nums){
+            x = i-1;
+            while(s.find(x) != s.end()){
+                prev++;
+                s.erase(x);
+                x--;
+            }
+            x = i+1;
+            while(s.find(x) != s.end()){
+                next++;
+                s.erase(x);
+                x++;
+            }
+            ans = max(ans,prev+next+1);
+            prev = 0,next = 0;
+        } 
+        return ans;
+    }
+};
