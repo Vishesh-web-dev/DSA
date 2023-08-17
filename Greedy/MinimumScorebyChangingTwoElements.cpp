99b84598de421c91b5637479aa406ab4d70d5fde
Vishesh Shrivastava <vishesh24cs138@satiengg.in>
2023-08-17 08:25:20 +0530

    Greedy Problem

--- Greedy/MinimumScorebyChangingTwoElements.cpp
+++ Greedy/MinimumScorebyChangingTwoElements.cpp
@@ -1,15 +1,13 @@
-//leetcode
 class Solution {
 public:
     int minimizeSum(vector<int>& nums) {
         //consider all the possible pairs of max and min 
         //after changing
-        sort(nums.begin(),nums.end());
         int n = nums.size();
-        int ans1 = abs(nums[1]-nums[n-2]);//second min and max
-        int ans2 = abs(nums[2]-nums[n-1]);//third min and first max
-        int ans3 = abs(nums[0]-nums[n-3]); //first min and third max
-        int ans = min(ans1,min(ans2,ans3));
-        return ans;
+        sort(nums.begin(),nums.end());
+        int poss1 = abs(nums[1]-nums[n-2]);//second min and max
+        int poss2 = abs(nums[2]-nums[n-1]);//third min and first max
+        int poss3 = abs(nums[0]-nums[n-3]); //first min and third max
+        return min({poss1,poss2,poss3});
     }
 };
