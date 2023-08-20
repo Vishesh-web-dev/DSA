cda59205cdb0118881bb77f8354494101baf3db6
Vishesh Shrivastava <vishesh24cs138@satiengg.in>
2023-08-20 19:01:26 +0530

    Some Changes

--- MATH/GCD.cpp
+++ MATH/GCD.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int gcd(int a ,int b){
     if(b == 0)return a;
+    // cout<<a<<" "<<b<<" \n"; //dry run
     return gcd(b,a%b);
 }
 // int gcd2(int a, int b){
@@ -17,6 +18,8 @@ int gcd(int a ,int b){
 int main(){
     int a,b;
     cin>>a>>b;
-    cout<<gcd(a,b);
+    cout<<gcd(a,b)<<"\n";
+    // cout<<gcd(b,a);
+    // cout<<"a%b=> "<<a%b<<" b%a=> "<<b%a;
     return 0;
 }
