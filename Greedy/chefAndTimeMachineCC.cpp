1a051a64738d89b3fcd9804bda88978a35203c43
Vishesh Shrivastava <vishesh24cs138@satiengg.in>
2023-09-05 10:50:56 +0530

    Greedy

---
+++ Greedy/chefAndTimeMachineCC.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+//macros
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpii vector<pair<int, int>>
+#define vb vector<bool>
+#define vs vector<string>
+#define forn(n) for (int i = 0; i < int(n); i++)
+#define pb push_back
+#define mkp make_pair
+#define ff first 
+#define ss second 
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define nl "\n"
+#define LL_INF (1LL << 62)
+#define INF (1 << 30)
+#define mod 100000007
+#define N 1000009
+#define all(v) v.begin(),v.end()
+#define srt(v)  if(!v.empty())sort(v.begin(),v.end())
+#define rev(v)  if(!v.empty())reverse(v.begin(),v.end())
+
+//inline functions and macros
+int expo(int a,int n){ int res=1; while(n){ if(n&1) {res *= a;--n;} else {a *= a;n >>= 1;}} return res;}
+ll pwr(ll a, ll b) {a %= mod; ll res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;} 
+ll inv(ll a){return pwr(a,mod-2);}
+ll mod_mul(ll a, ll b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+ll mod_add(ll a, ll b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+ll mod_div(ll a,ll b){return mod_mul(a,inv(b));}
+
+//Debug
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => ";_print(x);cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(int a){cerr << a;}
+void _print(ll a){cerr << a;}
+void _print(char a){cerr << a;}
+void _print(string a){cerr << a;}
+void _print(bool a){cerr << a;}
+template<class T> void _print(vector<T> v1){cerr << "[ ";for(T i : v1){_print(i);cerr <<" ";}cerr << "]";}
+template<class T> void _print(set<T> s1){cerr << "[ ";for(T i : s1){_print(i);cerr <<" ";}cerr << "]";}
+template<class T,class V> void _print(pair<T,V> p1){cerr << "[ ";_print(p1.ff);cerr <<" ";_print(p1.ss);cerr<<" ";cerr << "]";}
+template<class T,class V> void _print(map<T,V> m1){cerr << "\n[ ";for(auto i : m1){_print(i.ff);cerr <<" ";_print(i.ss);cerr<<" \n";}cerr << "]";}
+template<class T> void _print(vector<vector<T>> v1){cerr << "\n";for(vector<T>&  i : v1){_print(i);cerr <<"\n";}}
+
+void solve() {
+    int n,k,m;
+    cin>>n>>k>>m;
+    vi a(n),b(n),rem(n),red(k+m);
+    forn(n){cin>>a[i];}
+    forn(n){cin>>b[i];}
+    forn(n){rem[i] = a[i]-b[i];}
+    forn(k+m){cin>>red[i];}
+    
+    srt(rem);rev(rem);
+    srt(red);rev(red);
+
+    int i = 0, j = 0;
+    while(j < m+k && i < n){
+        if(rem[i] < red[j]){
+            j++;
+        }else{
+            rem[i++] -= red[j++];
+        }
+    }
+    ll ans = accumulate(all(rem),0);
+    debug(rem);
+    debug(red);
+    cout<<ans<<endl;
+}
+    
+int main() {
+ 
+#ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("Error.txt","w", stderr);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
